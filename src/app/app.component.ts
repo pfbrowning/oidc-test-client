87e31ce1dc67f137d46eef0f424e7946afb0cbde
Patrick Browning <patrick@browninglogic.com>
2018-12-19 23:44:59 -0500

    Show loading indicator while loading the discovery document

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoadingIndicatorService } from '@browninglogic/ng-loading-indicator';
 import { AuthenticationService } from './services/authentication.service';
 import { ErrorHandlingService } from './services/error-handling.service';
@@ -9,13 +9,19 @@ import { MessageService } from 'primeng/api';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'OIDC Test Client';
 
   constructor(private authenticationService: AuthenticationService,
     private loadingIndicatorService: LoadingIndicatorService,
     private errorHandlingService: ErrorHandlingService,
     private messageService: MessageService) {}
+
+  ngOnInit() {
+    // Show the loading indicator while loading the discovery document & processing the id token
+    this.loadingIndicatorService.showLoadingIndicator('Loading Discovery Document');
+    this.authenticationService.tokenProcessed().subscribe(() => this.loadingIndicatorService.hideLoadingIndicator());
+  }
 
   public get userAuthenticated(): boolean {
     return this.authenticationService.authenticated;
@@ -34,7 +40,10 @@ export class AppComponent {
     this.loadingIndicatorService.showLoadingIndicator('Performing Silent Refresh');
     this.authenticationService.silentRefresh()
       .then(() => this.messageService.add({severity: 'success', summary: 'Silent Refresh Successful'}))
-      .catch(error => this.errorHandlingService.handleError(error, 'Silent Refresh Failed'))
+      .catch(error => {
+        console.log('silent refresh error', error);
+        this.errorHandlingService.handleError(error, 'Silent Refresh Failed');
+      })
       .then(() => this.loadingIndicatorService.hideLoadingIndicator());
   }
 }
